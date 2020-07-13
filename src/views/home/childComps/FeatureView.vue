9d3bd694e74a60330c9eda2e7315ff0a2501e4ee
doudouTan <doudoutan6@gmail.com>
2020-07-13 11:43:50 +0800

    7月13日中午

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="feature">
+        <a href="https://act.mogujie.com/zzlx67"></a>
+        <img src="../../../assets/img/home/recommend_bg.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+      name:"FetureView",
+}
+</script>
+
+<style scoped>
+    .feature img{
+         width:100%;
+         height:100%
+    }
+</style>
