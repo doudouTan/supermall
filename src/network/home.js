90db77d3b1f8e69c3ec5d0246d606ef704f8a055
doudouTan <doudoutan6@gmail.com>
2020-07-17 01:33:14 +0800

    7月17日更新

--- src/network/home.js
+++ src/network/home.js
@@ -5,3 +5,13 @@ export function getHomeMultidata(){
         url:"http://152.136.185.210:8000/api/n3/home/multidata"
     })
 }
+export function getHomeGoods(type,page){
+    return  request({
+        url:'http://152.136.185.210:8000/api/n3/home/data',
+        params:{
+            type,page
+        }
+    
+    })
+
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,156 +1,83 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-     <home-swiper :banners="banners"></home-swiper>
-     <recommend-view :recommends="recommends"></recommend-view>
-     <feature-view></feature-view>
-     <ul>
-       <li>101</li>
-       <li>102</li>
-       <li>103</li>
-       <li>104</li>
-       <li>105</li>
-       <li>106</li>
-       <li>107</li>
-       <li>108</li>
-       <li>109</li>
-       <li>1010</li>
-       <li>1011</li>
-       <li>1012</li>
-       <li>1013</li>
-       <li>1014</li>
-       <li>1015</li>
-       <li>1016</li>
-       <li>1017</li>
-       <li>1018</li>
-       <li>1019</li>
-       <li>1020</li>
-       <li>1021</li>
-       <li>1022</li>
-       <li>1023</li>
-       <li>1024</li>
-       <li>1025</li>
-       <li>1026</li>
-       <li>1027</li>
-       <li>1028</li>
-       <li>1029</li>
-       <li>1030</li>
-       <li>1031</li>
-       <li>1032</li>
-       <li>1033</li>
-       <li>1034</li>
-       <li>1035</li>
-       <li>1036</li>
-       <li>1037</li>
-       <li>1038</li>
-       <li>1039</li>
-       <li>1040</li>
-       <li>1041</li>
-       <li>1042</li>
-       <li>1043</li>
-       <li>1044</li>
-       <li>1045</li>
-       <li>1046</li>
-       <li>1047</li>
-       <li>1048</li>
-       <li>1049</li>
-       <li>1050</li>
-       <li>1051</li>
-       <li>1052</li>
-       <li>1053</li>
-       <li>1054</li>
-       <li>1055</li>
-       <li>1056</li>
-       <li>1057</li>
-       <li>1058</li>
-       <li>1059</li>
-       <li>1060</li>
-       <li>1061</li>
-       <li>1062</li>
-       <li>1063</li>
-       <li>1064</li>
-       <li>1065</li>
-       <li>1066</li>
-       <li>1067</li>
-       <li>1068</li>
-       <li>1069</li>
-       <li>1070</li>
-       <li>1071</li>
-       <li>1072</li>
-       <li>1073</li>
-       <li>1074</li>
-       <li>1075</li>
-       <li>1076</li>
-       <li>1077</li>
-       <li>1078</li>
-       <li>1079</li>
-       <li>1080</li>
-       <li>1081</li>
-       <li>1082</li>
-       <li>1083</li>
-       <li>1084</li>
-       <li>1085</li>
-       <li>1086</li>
-       <li>1087</li>
-       <li>1088</li>
-       <li>1089</li>
-       <li>1090</li>
-       <li>1091</li>
-       <li>1092</li>
-       <li>1093</li>
-       <li>1094</li>
-       <li>1095</li>
-       <li>1096</li>
-       <li>1097</li>
-       <li>1098</li>
-       <li>1099</li>
-       <li>10100</li>
-     </ul>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['精选','流行','新款']"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list> 
   </div>
 </template>
 
 <script>
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
 import NavBar from "../../components/common/navbar/NavBar";
-import { getHomeMultidata } from "network/home";
-import HomeSwiper from './childComps/HomeSwiper'
-import RecommendView from './childComps/RecommendView'
-import FeatureView from './childComps/FeatureView'
+import TabControl from "../../components/content/tabControl/TabControl";
+import GoodsList from '../../components/content/goods/GoodsList'
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods:{
+        'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]}
+      }
     };
   },
   components: {
-    NavBar,
     HomeSwiper,
     RecommendView,
-    FeatureView
+    NavBar,
+    FeatureView,
+    TabControl,
+    GoodsList,
   },
   created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-      console.log(this.banners);
+    this.getHomeMultidata();
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type){
+       const page =this.goods[type].page+1
+       getHomeGoods(type, 1).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page +1;
     });
+    },
   }
 };
 </script>
 
 <style>
-#home{
-  padding-top:42px;
+#home {
+  padding-top: 42px;
 }
 .home-nav {
   background-color: var(--color-tint);
   color: white;
-  position:fixed;
+  position: fixed;
   z-index: 9;
-  top:0;
-  left:0;
-  right:0;
+  top: 0;
+  left: 0;
+  right: 0;
 }
 </style>
